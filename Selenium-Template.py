d9687f16fbdb549edca55f7eafcd583b792a00c3
Nothig00 <112175844+Nothig00@users.noreply.github.com>
2023-05-27 00:16:36 +0530

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -1,40 +1,29 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-import chromedriver_autoinstaller
-from pyvirtualdisplay import Display
-display = Display(visible=0, size=(800, 800))  
-display.start()
 
-chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
-                                      # and if it doesn't exist, download it automatically,
-                                      # then add chromedriver to path
 
-chrome_options = webdriver.ChromeOptions()    
-# Add your options as needed    
-options = [
-  # Define window size here
-   "--window-size=1200,1200",
-    "--ignore-certificate-errors"
- 
-    #"--headless",
-    #"--disable-gpu",
-    #"--window-size=1920,1200",
-    #"--ignore-certificate-errors",
-    #"--disable-extensions",
-    #"--no-sandbox",
-    #"--disable-dev-shm-usage",
-    #'--remote-debugging-port=9222'
-]
+import threading
 
-for option in options:
-    chrome_options.add_argument(option)
+import time
 
-    
-driver = webdriver.Chrome(options = chrome_options)
+maxthreads = 5
 
-driver.get('http://github.com')
-print(driver.title)
-with open('./GitHub_Action_Results.txt', 'w') as f:
-    f.write(f"This was written with a GitHub action {driver.title}")
+sema = threading.Semaphore(value=maxthreads)
 
+threads = list()
+
+def task(i):
+
+    sema.acquire()
+
+    print "start %s" % (i,)
+
+    time.sleep(2)
+
+    sema.release()
+
+for i in range(10):
+
+    thread = threading.Thread(target=task,args=(str(i)))
+
+    threads.append(thread)
+
+    thread.start(
